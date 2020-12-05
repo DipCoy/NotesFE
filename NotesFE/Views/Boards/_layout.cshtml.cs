b20e2443319f97e30df3581945657d437a55003c
vadik <vadikverein45@gmail.com>
2020-12-05 15:57:35 +0500

    Add Layout

---
+++ NotesFE/Views/Boards/_layout.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NotesFE.Views.Boards
+{
+    public class _layout : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
